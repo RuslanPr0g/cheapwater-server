8e73b56cc73fd63a04fa4b531cbdb462dad93a5f
Yaroslav Holota <korova2321@gmail.com>
2020-12-20 20:01:27 +0200

    Added dapper sql manager

---
+++ DataAccessLibrary/DB/DapperSQL/SQLDataAccess.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SQLDataAccess : ISQLDataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public string ConnectionStringName { get; } = "Standard";
+
+        public SQLDataAccess(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public async Task<List<T>> LoadDataNoParam<T>(string sql)
+        {
+            string connectionString = _configuration.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql);
+
+                return data.ToList();
+            }
+        }
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionString = _configuration.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+
+                return data.ToList();
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionString = _configuration.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
+
+    }
+}
