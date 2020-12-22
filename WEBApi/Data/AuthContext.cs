4163a09a09f81236820682dedef138e39ec6211b
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-22 22:51:02 +0300

    add context

---
+++ WEBApi/Data/AuthContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEBApi.Entities;
+
+namespace WEBApi.Data
+{
+    public class AuthContext : DbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
