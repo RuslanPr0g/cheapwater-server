d145417615d275fbf07b43e32b465e1b66352349
Yaroslav Holota <korova2321@gmail.com>
2020-12-22 15:48:32 +0200

    Added basic websocket functional

---
+++ WEBApi/Middleware/WebSocketsMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WEBApi.Middleware
+{
+    public class WebSocketsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WebSocketsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    
+                await Receive(webSocket, async (result, buffer) =>
+                {
+                    switch (result.MessageType)
+                    {
+                        case WebSocketMessageType.Text:
+                            //process text message
+                            break;
+                        case WebSocketMessageType.Binary:
+                            //process binary message
+                            break;
+                        case WebSocketMessageType.Close:
+                            //close the connection
+                            break;
+                    }
+                });
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                                                       cancellationToken: CancellationToken.None);
+                handleMessage(result, buffer);
+            }
+        }
+    }
+}
--- WEBApi/Startup.cs
+++ WEBApi/Startup.cs
@@ -53,6 +53,10 @@ namespace WEBApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WEBApi v1"));
             }
 
+            app.UseWebSockets();
+
+            app.UseWebSocketsServer();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
