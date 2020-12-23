a57a9bc127cfff87d027b573e0ece4e656a3627d
Yaroslav Holota <korova2321@gmail.com>
2020-12-23 19:14:43 +0200

    Updated dtos

--- DataAccessLibrary/DB/DTOS/UserModelBase.cs
+++ DataAccessLibrary/DB/DTOS/UserModelBase.cs
@@ -5,8 +5,6 @@ namespace DataAccessLibrary.DB.Models
     public class UserModelBase
     {
         [Required]
-        public string UserID { get; set; }
-        [Required]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
