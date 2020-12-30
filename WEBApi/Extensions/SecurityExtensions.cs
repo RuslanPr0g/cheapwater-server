fe8ca3ad28d612312ea144fad05dbaf1d3df1337
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-30 19:47:01 +0300

    add method to security extentions class to add encryption

--- WEBApi/Extensions/SecurityExtensions.cs
+++ WEBApi/Extensions/SecurityExtensions.cs
@@ -1,9 +1,17 @@
-﻿using System.Linq;
+﻿using DataAccessLibrary.Encryption;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography;
+
 
 namespace WEBApi.Extensions
 {
     public static class SecurityExtensions
     {
-        
+        public static IServiceCollection AddEncryption(this IServiceCollection services)
+        {
+            services.AddSingleton<HashAlgorithm>(MD5.Create());
+            services.AddSingleton<IEncrypter, Encrypter>();
+            return services;
+        }
     }
 }
