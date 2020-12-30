7c559a63552d7edd9f3106f8711a1d1d7c3763d5
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-30 19:43:04 +0300

    add method to add dapper database to data access extentions

--- WEBApi/Extensions/DataAccessExtentions.cs
+++ WEBApi/Extensions/DataAccessExtentions.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using DataAccessLibrary;
+using DataAccessLibrary.DB;
 
 namespace WEBApi.Extensions
 {
     public static class DataAccessExtentions
     {
-
+        public static IServiceCollection AddDapperDatabase(this IServiceCollection services)
+        {
+            services.AddSingleton<ISQLDataAccess, SQLDataAccess>();
+            services.AddSingleton<IUserReadRepo, UserReadRepo>();
+            return services;
+        }
     }
 }
