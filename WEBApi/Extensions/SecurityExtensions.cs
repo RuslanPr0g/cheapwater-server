51b52289d27202ff1dfb89fa2f44849d442fe4d4
RuslanPr0g <theruslan.prog@gmail.com>
2020-12-30 19:50:48 +0300

    rearrange jwt extentions from its class to brand new security one

--- WEBApi/Extensions/SecurityExtensions.cs
+++ WEBApi/Extensions/SecurityExtensions.cs
@@ -1,6 +1,11 @@
 ﻿using DataAccessLibrary.Encryption;
 using Microsoft.Extensions.DependencyInjection;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WEBApi.Authentication;
 
 
 namespace WEBApi.Extensions
@@ -11,6 +16,34 @@ namespace WEBApi.Extensions
         {
             services.AddSingleton<HashAlgorithm>(MD5.Create());
             services.AddSingleton<IEncrypter, Encrypter>();
+            return services;
+        }
+
+        public static IServiceCollection AddJWTokens(this IServiceCollection services, IConfiguration conf)
+        {
+            services.AddSingleton<IJwtokenManagerFactory, JwtokenManagerFactory>();
+
+            string key = conf.GetValue<string>("Key");
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddSingleton<IJWTokenManager>(x => x.GetService<IJwtokenManagerFactory>().CreateTokenManager());
+
             return services;
         }
     }
