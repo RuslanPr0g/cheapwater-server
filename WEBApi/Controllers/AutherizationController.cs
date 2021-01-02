12f8bdca3da8a7280b10e3b17c3e2e642c3fb555
Yaroslav Holota <korova2321@gmail.com>
2021-01-02 14:31:01 +0200

    Removed validation from controller

--- WEBApi/Controllers/AutherizationController.cs
+++ WEBApi/Controllers/AutherizationController.cs
@@ -12,6 +12,7 @@ using System.Threading;
 using MediatR;
 using WEBApi.CQRS.Actions.Commands;
 using WEBApi.CQRS.Actions.Queries;
+using FluentValidation;
 
 namespace WEBApi.Controllers
 {
@@ -33,20 +34,6 @@ namespace WEBApi.Controllers
         {
             try
             {
-                var results = await _validator.ValidateAsync(userDto, cancellation);
-
-                if (!results.IsValid)
-                {
-                    List<string> ErrorMessages = new List<string>();
-                    foreach (var Error in results.Errors)
-                    {
-                        ErrorMessages.Add(Error.ErrorMessage);
-                    }
-                    return BadRequest(ErrorMessages);
-                }
-
-                cancellation.ThrowIfCancellationRequested();
-
                 var command = new RegistrationCommand(userDto);
 
                 string token = await _mediator.Send(command, cancellationToken: cancellation);
@@ -62,9 +49,14 @@ namespace WEBApi.Controllers
             {
                 return BadRequest("Canceled");
             }
-            catch (Exception e)
+            catch(ValidationException exc)
             {
-                return BadRequest("Something went wrong: " + e.Message);
+                return BadRequest(exc.Message);
+            }
+            catch(Exception exc)
+            {
+                //log
+                return BadRequest();
             }
         }
 
