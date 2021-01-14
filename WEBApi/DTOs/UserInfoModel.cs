46c79b22f81c9e3583d9405879f3318009da7c07
Yaroslav Holota <korova2321@gmail.com>
2021-01-14 10:27:10 +0200

    Removed unnecessary usings

--- WEBApi/DTOs/UserInfoModel.cs
+++ WEBApi/DTOs/UserInfoModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WEBApi.DTOs
+﻿namespace WEBApi.DTOs
 {
     public class UserInfoModel
     {
